dd4775b035bcae886aebb9d5176ee8e30dd5919f
Jorge Matos <jisapjisap@gmail.com>
2025-01-29 17:12:15 +0100

    Add session management to layouts for authentication flow

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import Header from '@/components/Header'
+import { redirect } from 'next/navigation'
+import { auth } from '@/auth'
 
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async({ children }: { children: React.ReactNode }) => {
+
+  const session = await auth()
+  if(!session) redirect('/sign-in')
+
   return (
     <main className='root-container'>
       <div className='mx-auto max-w-7xl'>
